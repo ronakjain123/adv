3b4aab471f7c691017e85bb798c0b86be053089b
jain_ro <ronak.jain@xoriant.com>
2016-12-09 20:31:07 +0530

    for submitAd

--- src/js/controllers/submitAdController.js
+++ src/js/controllers/submitAdController.js
@@ -1,12 +1,34 @@
 /**
  * Created by jain_ro on 11/28/2016.
  */
+
 var app = angular.module('advApp')
 
-app.controller('submitAdController', ['$scope', 'submitAdService', function($scope, submitAdService) {
+app.controller('submitAdController', ['$rootScope', '$scope', 'submitAdService', '$location', function($rootScope, $scope, submitAdService, $location) {
+    console.log('submitAd ctrl.......');
+    $scope.user = {};
+    $scope.submitAd = function() {
 
-    $scope.submitAd= function() {
+        var res = submitAdService.submitAd($scope.user);
+        res.success(function (data, status, headers, config) {
+            var res1 = JSON.stringify(data);
 
-        submitAdService.submitAd();
-    }
+            console.log(data);
+            console.log(data.data);
+
+                if(data.data.userId) {
+                    $location.path('/home');
+                    $rootScope.userdata = data.data;
+
+                    console.log($rootScope);
+                }else {
+                    console.log('Failed...');
+                    console.log(data.data);
+                    $location.path('/login');
+                    $rootScope.userdata = [];
+                }
+            })
+            .error(function (data, status, header, config) {
+            });
+    };
 }]);
--- src/js/services/submitAdService.js
+++ src/js/services/submitAdService.js
@@ -1,17 +1,11 @@
 /**
  * Created by jain_ro on 11/28/2016.
  */
+
 var app = angular.module('advApp')
 
-app.service('submitAdService', ['$http', function($http) {
-    this.submitAd = function() {
-        $http.post({url: 'http://10.20.14.83:9000/postAd', method: 'JSONP'}).then({
-            'success': function() {
-                console.log('Success12..........');
-            },
-            'failure': function() {
-                console.log('Failed12..........');
-            }
-        })
+app.service('submitAdService', ['$http','$location', function($http, $location) {
+    this.submitAd = function(userdata) {
+        return $http.post('http://10.20.14.83:9000/postAd', userdata);
     }
 }]);
